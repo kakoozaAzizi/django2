29a33ee89a136e4783a297a1a581f3c4eaf75818
kulubasi <61836873+kulubasi@users.noreply.github.com>
2022-09-12 15:18:35 +0300

    Now works properly

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from myapp.models import *
+from myapp.models import temperature,pH
 # Register your models here.
 
 
--- myapp/models.py
+++ myapp/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 # Create your models here.
-from multiprocessing.sharedctypes import Value
+# from multiprocessing.sharedctypes import Value
 from django.db import models
 
 # Create your models here.
 
 class temperature(models.Model):
-    Value=models.CharField()
+    Value=models.CharField(max_length=50)
     timestamp=models.DateTimeField(auto_now_add=True)
     comments=models.CharField(max_length=50)
 
@@ -15,7 +15,7 @@ class temperature(models.Model):
         return self.title
 
 class pH(models.Model):
-    Value=models.CharField()
+    Value=models.CharField(max_length=50)
     timestamp=models.DateTimeField(auto_now_add=True)
     comments=models.CharField(max_length=50)
 
